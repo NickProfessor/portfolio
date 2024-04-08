37803cdf6d9724f079b6634d07137eda319505ed
Nickollas <nickollassilva90@gmail.com>
2024-04-08 12:34:03 -0300

    criando a seção inicio para mobile

--- scripts/menu.js
+++ scripts/menu.js
@@ -6,3 +6,30 @@ btnMenu.addEventListener('click', () => {
     document.querySelector('#desenho-barras').classList.toggle('fa-rotate-90');
 })
 
+
+
+// Seleciona o botão no header
+const meuBotao = document.querySelector('.cabecalho__btn-hamburguer');
+// Seleciona todas as seções
+const secao = document.querySelector('.secao__inicio');
+
+// Função para verificar a posição do scroll
+function verificarPosicaoScroll() {
+    // Obtém a posição atual do scroll
+    const posicaoScroll = window.scrollY;
+    
+    // Verifica em qual seção o usuário está
+        const secaoTopo = secao.offsetTop;
+        const secaoBottom = secaoTopo + secao.offsetHeight;
+        
+        if (posicaoScroll >= secaoTopo && posicaoScroll < secaoBottom) {
+            // Se o usuário está dentro da seção, mostra o botão
+            meuBotao.style.display = 'none';
+        } else {
+            // Se o usuário não está na seção, oculta o botão
+            meuBotao.style.display = 'block';
+    }
+}
+
+// Adiciona um evento de rolagem para verificar a posição do scroll
+window.addEventListener('scroll', verificarPosicaoScroll);
